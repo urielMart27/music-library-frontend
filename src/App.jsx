331ecff9d7a7754d7532749689d24d533493af26
Uriel Martinez <martinezuriel651@gmail.com>
2023-12-15 11:43:04 -0600

    Installed Axios and configured CORS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import './App.css';
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const fetchSongs = async () => {
+    try {
+      const response = await axios.get("https://localhost:7073/api/Songs");
+      console.log(response);
+    } catch (error) {
+      console.warn("Error in Fetch Songs Request: ", error);
+    }
+  };
   return (
     <div className="App">
-      
+      <button onClick={fetchSongs}>Fetch Songs</button>
     </div>
   );
 }
